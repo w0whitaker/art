aa046161cacc88ab429ea3cd8f9842c8a309450a
William Whitaker <william@williamwhitaker.dev>
2024-11-26 06:32:14 -0500

    add eleventy navigation

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,16 @@
+import pluginNavigation from "@11ty/eleventy-navigation";
+
 export default function (eleventyConfig) {
+    // Plugins
+    eleventyConfig.addPlugin(pluginNavigation);
+
     eleventyConfig.addPassthroughCopy("public/");
 
     return {
         dir: {
             input: "content",
             output: "_site",
-            data: "_data",
+            data: "../_data",
             includes: "../_includes", // relative to 'input' dir
         },
     };
