9503079dfd64bee29d87fb9c3dda10bab4c2dc86
William Whitaker <william@williamwhitaker.dev>
2024-11-26 06:23:26 -0500

    add paintings to data

--- content/paintings/paintings.11tydata.js
+++ content/paintings/paintings.11tydata.js
@@ -1,18 +1,90 @@
 export const paintings = [
     {
-        title: "number 1",
+        title: "ULG2005",
+        file: "/public/ULG2005.webp",
+    },
+    {
+        title: "ULG2006",
+        file: "/public/ULG2006.webp",
+    },
+    {
+        title: "ULG2007",
+        file: "/public/ULG2007.webp",
+    },
+    {
+        title: "ULG2093",
+        file: "/public/ULG2093.webp",
+    },
+    {
+        title: "ULG2094",
+        file: "/public/ULG2094.webp",
+    },
+    {
+        title: "ULG2095",
+        file: "/public/ULG2095.webp",
+    },
+    {
+        title: "ULG2096",
+        file: "/public/ULG2096.webp",
+    },
+    {
+        title: "ULG2097",
+        file: "/public/ULG2097.webp",
+    },
+    {
+        title: "ULG2101",
+        file: "/public/ULG2101.webp",
+    },
+    {
+        title: "ULG2102",
+        file: "/public/ULG2102.webp",
+    },
+    {
+        title: "ULG2103",
+        file: "/public/ULG2103.webp",
+    },
+    {
+        title: "ULG2104",
+        file: "/public/ULG2104.webp",
+    },
+    {
+        title: "ULG2105",
+        file: "/public/ULG2105.webp",
+    },
+    {
+        title: "ULG2106",
+        file: "/public/ULG2106.webp",
+    },
+    {
+        title: "ULG2107",
         file: "/public/ULG2107.webp",
     },
     {
-        title: "number 2",
+        title: "ULG2108",
+        file: "/public/ULG2108.webp",
     },
     {
-        title: "number 3",
+        title: "ULG2109",
+        file: "/public/ULG2109.webp",
     },
     {
-        title: "number 4",
+        title: "ULG2110",
+        file: "/public/ULG2110.webp",
     },
     {
-        title: "number 5",
+        title: "ULG2111",
+        file: "/public/ULG2111.webp",
+    },
+    {
+        title: "ULG2112",
+        file: "/public/ULG2112.webp",
+    },
+    {
+        title: "ULG2113",
+        file: "/public/ULG2113.webp",
+    },
+    {
+        title: "ULG2114",
+        file: "/public/ULG2114.webp",
     },
 ];
