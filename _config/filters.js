5b9d0a67c1caf85e998195106611fb7f80d447bc
William Whitaker <william@williamwhitaker.dev>
2024-12-01 09:20:35 -0500

    add titlecase filter

---
+++ _config/filters.js
@@ -0,0 +1,7 @@
+export default function (eleventyConfig) {
+    eleventyConfig.addFilter("titleCase", function (str) {
+        let strOut =
+            str.charAt(0).toUpperCase() + str.slice(1).toLowerCase() + ".";
+        return strOut;
+    });
+}
--- _data/metadata.js
+++ _data/metadata.js
@@ -1,4 +1,4 @@
 export default {
     displayName: "w. whitaker",
-    title: "Paintings.",
+    title: "paintings",
 };
